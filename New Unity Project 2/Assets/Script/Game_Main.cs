e71e456ec0fb406f7e08425cd70c1c7873af2178
Shikinami_Toshiki <mgs_with_cqc@i.softbank.jp>
2014-03-09 23:44:57 +0900

    [4PL-F01]ところ構わずではありますがクリックした位置に駒を置けるようにしました。

---
+++ New Unity Project 2/Assets/Script/Game_Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Main : MonoBehaviour {
+	public GameObject P_STONE;
+	public int[,] Banmen=new int[8,8];
+	// Use this for initialization
+	void SetUp (int Size) {
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 Pos = new Vector3 (0.0f, 0.0f, 0.0f);
+		Vector3 vec = Input.mousePosition;
+		vec = camera.ScreenToWorldPoint (vec);
+		vec.z = 0.0f;
+		if (Input.GetMouseButtonDown (0)) {
+			Instantiate (this.P_STONE, vec, Quaternion.identity);
+		}
+	}
+}
---
+++ New Unity Project 2/Assets/Script/Panel_Back.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+//背景のパネルという事で持たせるべき情報は
+//・背景ID
+//といったところでしょうか
+//
+public class Panel_Back : MonoBehaviour {
+
+	public float ID_Back=0;//背景のIDです
+	//この変数で背景だとか盤面の変更を行います
+	//どうやらfloat型でしかやり取りを行えないメソッドがあるらしいのでそれに備えてfloat型にしています
+	
+	//初期化処理ってやつですね
+	//このLoadメソッドで背景のIDを取得しそれに応じた画像に設定してやります
+	void Load (float ID) {
+		switch ((int)ID) {
+		case 0:
+			break;
+		default:
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	//背景で毎フレすべき様な処理が見当たらないので空になってます
+	void Update () {
+	
+	}
+}
