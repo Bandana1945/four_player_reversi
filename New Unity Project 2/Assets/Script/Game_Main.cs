efc1fd18bb8ea29aafc5b7b636dfbd4b0c6772c6
Shikinami_Toshiki <mgs_with_cqc@i.softbank.jp>
2014-03-10 10:02:00 +0900

    [4PL-F01]マス目に沿って駒を置けるようにしました。

--- New Unity Project 2/Assets/Script/Game_Main.cs
+++ New Unity Project 2/Assets/Script/Game_Main.cs
@@ -1,23 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Game_Main : MonoBehaviour {
 	public GameObject P_STONE;
 	public int[,] Banmen=new int[8,8];
 	// Use this for initialization
-	void SetUp (int Size) {
-
+	//盤面の初期化を行います
+	void Start () {
+		for(int i=0;i<8;i++)
+		{
+			for(int j=0;j<8;j++)
+			{
+				Banmen[i,j]=0;
+			}
+		}
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 Pos = new Vector3 (0.0f, 0.0f, 0.0f);
+		Set_Stone ();
+	}
+	void Set_Stone()
+	{
 		Vector3 vec = Input.mousePosition;
+		Vector3 Pos;
 		vec = camera.ScreenToWorldPoint (vec);
 		vec.z = 0.0f;
+		Pos.x = -18.0f;
+		Pos.y = 23.5f;
+		Pos.z = 0.0f;
+		int Board_X = (int)(Math.Floor ((vec.x + 18+2.25)) / 5.1);
+		int Board_Y = (int)(Math.Floor ((vec.y - 23.5-2.25)) / 5.1 * -1);
 		if (Input.GetMouseButtonDown (0)) {
-			Instantiate (this.P_STONE, vec, Quaternion.identity);
+			if(Board_X>=0&&Board_X<=7&&Board_Y>=0&&Board_Y<=7)
+			{
+				if(Banmen[Board_X,Board_Y]==0)
+				{
+					Pos.x=Pos.x+(5.1f*Board_X);
+					Pos.y=Pos.y-(5.1f*Board_Y);
+					Instantiate (this.P_STONE, Pos, Quaternion.identity);
+					Banmen[Board_X,Board_Y]=1;
+				}
+			}
 		}
 	}
 }
